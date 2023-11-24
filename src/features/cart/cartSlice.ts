241ad57803219e7f8efed3ddb3659d8ac6d10a9a
MyoniM <se.yonatan.merkebu@gmail.com>
2023-11-25 01:13:35 +0300

    Implemented resposnive pages for cart/product/product-detail

---
+++ src/features/cart/cartSlice.ts
@@ -0,0 +1,46 @@
+"use client";
+import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
+import type { RootState } from "@/redux/store";
+import { CartItem, CartState } from "./types";
+// Define the initial state using that type
+const initialState: CartState = {
+  items: [],
+};
+export const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addToCart: (state, action: PayloadAction<number>) => {
+      const id = action.payload;
+      const existingItem = state.items.find((item) => item.id === id);
+      if (existingItem) {
+        existingItem.quantity++;
+      } else {
+        state.items.push({ id, quantity: 1 });
+      }
+    },
+    removeFromCart: (state, action: PayloadAction<number>) => {
+      const id = action.payload;
+      const existingItem = state.items.find((item) => item.id === id);
+      if (existingItem) {
+        if (existingItem.quantity === 1) {
+          state.items = state.items.filter((item) => item.id !== id);
+        } else {
+          existingItem.quantity--;
+        }
+      }
+    },
+    checkout: (state) => {
+      // Simulate a checkout by clearing the cart.
+      state.items = [];
+    },
+  },
+});
+
+export const { addToCart, removeFromCart, checkout } = cartSlice.actions;
+export const totalCartItems = (state: RootState): number =>
+  state.cart.items.reduce((total, item) => total + item.quantity, 0);
+export const selectCartItems = (state: RootState): CartItem[] =>
+  state.cart.items;
+export default cartSlice.reducer;
---
+++ src/features/product/productAPI.ts
@@ -0,0 +1,27 @@
+"use client";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import type { Product } from "./types";
+export const productAPI = createApi({
+  reducerPath: "productAPI",
+  baseQuery: fetchBaseQuery({ baseUrl: "https://fakestoreapi.com/products/" }),
+  endpoints: (builder) => ({
+    getProductById: builder.query<Product, number>({
+      query: (id) => `${id}`,
+    }),
+    getAllProducts: builder.query<Product[], void>({
+      query: () => ``,
+    }),
+    getAllCategories: builder.query<string[], void>({
+      query: () => `categories`,
+    }),
+    getProductsInCategory: builder.query<Product[], string>({
+      query: (categoryName) => `category/${categoryName}`,
+    }),
+  }),
+});
+export const {
+  useGetProductByIdQuery,
+  useGetAllProductsQuery,
+  useGetAllCategoriesQuery,
+  useGetProductsInCategoryQuery,
+} = productAPI;
